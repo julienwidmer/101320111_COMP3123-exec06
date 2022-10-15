8a19b4b2b797c0e12f7a123e133f1aa93cb7517c
Julien Widmer <ju.widmer@pm.me>
2022-10-15 18:05:47 -0400

    Add Note JSON example

--- models/NotesModel.js
+++ models/NotesModel.js
@@ -12,7 +12,18 @@ const NotesSchema = new mongoose.Schema({
     dateUpdated: Date
 })
 
+/*
+JSON example:
+{
+    "noteTitle": "Hello World",
+    "noteDescription": "Lorem Ipsum Dolor Sit Amet",
+    "priority": "LOW",
+    "dateAdded": "2022-10-13T12:24:17.501Z",
+    "dateUpdated": "2022-10-15T00:24:17.501Z"
+}
+*/
+
 // Create mongodb schema
-const Notes = mongoose.model("Notes", NotesSchema)
+const Notes = mongoose.model("Notes", NotesSchema);
 // Export module
-module.exports = Notes
+module.exports = Notes;
